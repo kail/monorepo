69e2fa370da0fa09656da397e7f1118f707fbaf0
Mikhail Skobov <mskobov@lyft.com>
2021-12-27 22:27:00 -0800

    Getting to blinky

--- hw/snoot/fw/app/main.c
+++ hw/snoot/fw/app/main.c
@@ -1,5 +1,22 @@
+#include "hal_config.h"
+#include <stdbool.h>
+
+void blink() {
+  static bool led_on_ = false;
+  if (led_on_) {
+		HAL_GPIO_WritePin(GPIOA, GPIO_PIN_1, GPIO_PIN_RESET);
+	} else {
+		HAL_GPIO_WritePin(GPIOA, GPIO_PIN_1, GPIO_PIN_SET);
+	}
+	led_on_ = !led_on_;
+}
+
 int main() {
+  hal_init();
+
+  // Spin
   while (1) {
-    // Spin
+    blink();
+    HAL_Delay(1000);
   }
 }
