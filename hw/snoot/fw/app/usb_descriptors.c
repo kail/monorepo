5927d6d54a19ffe7c2d285542449d53ca41cb67a
Mikhail Skobov <mskobov@lyft.com>
2022-04-03 13:48:59 -0700

    cleanup

--- hw/snoot/fw/app/usb_descriptors.c
+++ hw/snoot/fw/app/usb_descriptors.c
@@ -49,7 +49,7 @@ tusb_desc_device_t const desc_device =
 
     // Use Interface Association Descriptor (IAD) for CDC
     // As required by USB Specs IAD's subclass must be common class (2) and protocol must be IAD (1)
-    .bDeviceClass       = TUSB_CLASS_CDC, //TUSB_CLASS_MISC,
+    .bDeviceClass       = TUSB_CLASS_CDC,
     .bDeviceSubClass    = MISC_SUBCLASS_COMMON,
     .bDeviceProtocol    = MISC_PROTOCOL_IAD,
     .bMaxPacketSize0    = CFG_TUD_ENDPOINT0_SIZE,
@@ -79,44 +79,18 @@ enum
 {
   ITF_NUM_CDC_0 = 0,
   ITF_NUM_CDC_0_DATA,
-  //ITF_NUM_CDC_1,
-  //ITF_NUM_CDC_1_DATA,
   ITF_NUM_TOTAL
 };
 
 #define CONFIG_TOTAL_LEN    (TUD_CONFIG_DESC_LEN + CFG_TUD_CDC * TUD_CDC_DESC_LEN)
 
-#if CFG_TUSB_MCU == OPT_MCU_LPC175X_6X || CFG_TUSB_MCU == OPT_MCU_LPC177X_8X || CFG_TUSB_MCU == OPT_MCU_LPC40XX
-  // LPC 17xx and 40xx endpoint type (bulk/interrupt/iso) are fixed by its number
-  // 0 control, 1 In, 2 Bulk, 3 Iso, 4 In etc ...
-  #define EPNUM_CDC_0_NOTIF   0x81
-  #define EPNUM_CDC_0_OUT     0x02
-  #define EPNUM_CDC_0_IN      0x82
+#define EPNUM_CDC_0_NOTIF   0x81
+#define EPNUM_CDC_0_OUT     0x02
+#define EPNUM_CDC_0_IN      0x82
 
-  #define EPNUM_CDC_1_NOTIF   0x84
-  #define EPNUM_CDC_1_OUT     0x05
-  #define EPNUM_CDC_1_IN      0x85
-
-#elif CFG_TUSB_MCU == OPT_MCU_SAMG || CFG_TUSB_MCU ==  OPT_MCU_SAMX7X
-  // SAMG & SAME70 don't support a same endpoint number with different direction IN and OUT
-  //    e.g EP1 OUT & EP1 IN cannot exist together
-  #define EPNUM_CDC_0_NOTIF   0x81
-  #define EPNUM_CDC_0_OUT     0x02
-  #define EPNUM_CDC_0_IN      0x83
-
-  #define EPNUM_CDC_1_NOTIF   0x84
-  #define EPNUM_CDC_1_OUT     0x05
-  #define EPNUM_CDC_1_IN      0x86
-
-#else
-  #define EPNUM_CDC_0_NOTIF   0x81
-  #define EPNUM_CDC_0_OUT     0x02
-  #define EPNUM_CDC_0_IN      0x82
-
-  #define EPNUM_CDC_1_NOTIF   0x83
-  #define EPNUM_CDC_1_OUT     0x04
-  #define EPNUM_CDC_1_IN      0x84
-#endif
+#define EPNUM_CDC_1_NOTIF   0x83
+#define EPNUM_CDC_1_OUT     0x04
+#define EPNUM_CDC_1_IN      0x84
 
 uint8_t const desc_fs_configuration[] =
 {
@@ -125,63 +99,7 @@ uint8_t const desc_fs_configuration[] =
 
   // 1st CDC: Interface number, string index, EP notification address and size, EP data address (out, in) and size.
   TUD_CDC_DESCRIPTOR(ITF_NUM_CDC_0, 4, EPNUM_CDC_0_NOTIF, 8, EPNUM_CDC_0_OUT, EPNUM_CDC_0_IN, 64),
-
-  // 2nd CDC: Interface number, string index, EP notification address and size, EP data address (out, in) and size.
-  // TUD_CDC_DESCRIPTOR(ITF_NUM_CDC_1, 4, EPNUM_CDC_1_NOTIF, 8, EPNUM_CDC_1_OUT, EPNUM_CDC_1_IN, 64),
 };
-
-// #if TUD_OPT_HIGH_SPEED
-// // Per USB specs: high speed capable device must report device_qualifier and other_speed_configuration
-
-// uint8_t const desc_hs_configuration[] =
-// {
-//   // Config number, interface count, string index, total length, attribute, power in mA
-//   TUD_CONFIG_DESCRIPTOR(1, ITF_NUM_TOTAL, 0, CONFIG_TOTAL_LEN, 0x00, 100),
-
-//   // 1st CDC: Interface number, string index, EP notification address and size, EP data address (out, in) and size.
-//   TUD_CDC_DESCRIPTOR(ITF_NUM_CDC_0, 4, EPNUM_CDC_0_NOTIF, 8, EPNUM_CDC_0_OUT, EPNUM_CDC_0_IN, 512),
-
-//   // 2nd CDC: Interface number, string index, EP notification address and size, EP data address (out, in) and size.
-//   // TUD_CDC_DESCRIPTOR(ITF_NUM_CDC_1, 4, EPNUM_CDC_1_NOTIF, 8, EPNUM_CDC_1_OUT, EPNUM_CDC_1_IN, 512),
-// };
-
-// // device qualifier is mostly similar to device descriptor since we don't change configuration based on speed
-// tusb_desc_device_qualifier_t const desc_device_qualifier =
-// {
-//   .bLength            = sizeof(tusb_desc_device_t),
-//   .bDescriptorType    = TUSB_DESC_DEVICE,
-//   .bcdUSB             = USB_BCD,
-
-//   .bDeviceClass       = TUSB_CLASS_MISC,
-//   .bDeviceSubClass    = MISC_SUBCLASS_COMMON,
-//   .bDeviceProtocol    = MISC_PROTOCOL_IAD,
-
-//   .bMaxPacketSize0    = CFG_TUD_ENDPOINT0_SIZE,
-//   .bNumConfigurations = 0x01,
-//   .bReserved          = 0x00
-// };
-
-// // Invoked when received GET DEVICE QUALIFIER DESCRIPTOR request
-// // Application return pointer to descriptor, whose contents must exist long enough for transfer to complete.
-// // device_qualifier descriptor describes information about a high-speed capable device that would
-// // change if the device were operating at the other speed. If not highspeed capable stall this request.
-// uint8_t const* tud_descriptor_device_qualifier_cb(void)
-// {
-//   return (uint8_t const*) &desc_device_qualifier;
-// }
-
-// // Invoked when received GET OTHER SEED CONFIGURATION DESCRIPTOR request
-// // Application return pointer to descriptor, whose contents must exist long enough for transfer to complete
-// // Configuration descriptor in the other speed e.g if high speed then this is for full speed and vice versa
-// uint8_t const* tud_descriptor_other_speed_configuration_cb(uint8_t index)
-// {
-//   (void) index; // for multiple configurations
-
-//   // if link speed is high return fullspeed config, and vice versa
-//   return (tud_speed_get() == TUSB_SPEED_HIGH) ?  desc_fs_configuration : desc_hs_configuration;
-// }
-
-// #endif // highspeed
 
 // Invoked when received GET CONFIGURATION DESCRIPTOR
 // Application return pointer to descriptor
@@ -189,13 +107,7 @@ uint8_t const desc_fs_configuration[] =
 uint8_t const * tud_descriptor_configuration_cb(uint8_t index)
 {
   (void) index; // for multiple configurations
-
-// #if TUD_OPT_HIGH_SPEED
-//   // Although we are highspeed, host may be fullspeed.
-//   return (tud_speed_get() == TUSB_SPEED_HIGH) ?  desc_hs_configuration : desc_fs_configuration;
-// #else
   return desc_fs_configuration;
-//#endif
 }
 
 //--------------------------------------------------------------------+
@@ -207,7 +119,7 @@ char const* string_desc_arr [] =
 {
   (const char[]) { 0x09, 0x04 }, // 0: is supported language is English (0x0409)
   "TinyUSB",                     // 1: Manufacturer
-  "TinyUSB Device",              // 2: Product
+  "Snoot",                       // 2: Product
   "123456",                      // 3: Serials, should use chip ID
   "TinyUSB CDC",                 // 4: CDC Interface
 };
